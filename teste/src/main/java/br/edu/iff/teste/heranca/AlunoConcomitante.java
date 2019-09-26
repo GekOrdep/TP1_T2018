7715fc15b7767c61dbdff007070f3b5649a70275
Joycekellylima <joycekellyalves7@gmail.com>
2019-09-26 09:56:39 -0300

    Aluno concomitante atualizado.

--- teste/src/main/java/br/edu/iff/teste/heranca/AlunoConcomitante.java
+++ teste/src/main/java/br/edu/iff/teste/heranca/AlunoConcomitante.java
@@ -9,9 +9,12 @@ public class AlunoConcomitante extends Aluno {
     long matriculaEM;
     String escolaEM;
     boolean apresentouCertificadoEM;
+    boolean apresentouDocsDeMatricula;
+    boolean concluiuTodasAsDisciplinas;
+    boolean nadaConsta;
     
     public boolean podeSolicitarDiploma() {
-        if (apresentouCertificadoEM) {
+        if (apresentouCertificadoEM && apresentouDocsDeMatricula && concluiuTodasAsDisciplinas && nadaConsta) {
             return true;
         } else {
             return false;
